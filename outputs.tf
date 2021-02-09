752f0aa1f6fd64c69586f891a6fc0b847e20237e
joni <joni@altnext.com>
2021-02-09 12:41:19 +0200

    Fix terraform warnings [#2]

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "exit_status" {
-  value = "${data.external.shell.result["exit_status"]}"
+  value = data.external.shell.result["exit_status"]
 }
 
 output "pid" {
-  value = "${data.external.shell.result["pid"]}"
+  value = data.external.shell.result["pid"]
 }
 
 output "stdout" {
-  value = "${data.external.shell.result["stdout"]}"
+  value = data.external.shell.result["stdout"]
 }
 
 output "stderr" {
-  value = "${data.external.shell.result["stderr"]}"
+  value = data.external.shell.result["stderr"]
 }
